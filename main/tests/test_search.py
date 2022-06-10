63d37b17bebf082e891eb0670c86771482d1a97e
Maxime Bellec <maxbellec2@gmail.com>
2022-06-10 16:48:20 +0000

    Search possible tags

--- main/tests/test_search.py
+++ main/tests/test_search.py
@@ -1,76 +1,197 @@
 from django.contrib.auth.models import AnonymousUser
 from django.test import TestCase
+from django.urls import reverse
 
-from main.factories import ResourceFactory, BaseFactory
+from main.factories import ResourceFactory, BaseFactory, TagFactory
 from main.search import search_resources, search_bases
 from main.tests.test_utils import authenticate
+from main.views.search_view import SearchView
 
 
 class TestSearch(TestCase):
     def test_anonymous_search_on_resources_public_data(self):
         ResourceFactory.create(title="MyTitle", is_public=True)
-        self.assertEqual(search_resources(AnonymousUser(), "MyTitle").count(), 1)
-        self.assertEqual(search_resources(AnonymousUser(), "OtherTitle").count(), 0)
+        self.assertEqual(
+            search_resources(AnonymousUser(), "MyTitle")["queryset"].count(), 1
+        )
+        self.assertEqual(
+            search_resources(AnonymousUser(), "OtherTitle")["queryset"].count(), 0
+        )
 
     def test_anonymous_search_on_resources_private_data(self):
         ResourceFactory.create(title="MyTitle", is_public=False)
-        self.assertEqual(search_resources(AnonymousUser(), "MyTitle").count(), 0)
-        self.assertEqual(search_resources(AnonymousUser(), "OtherTitle").count(), 0)
+        self.assertEqual(
+            search_resources(AnonymousUser(), "MyTitle")["queryset"].count(), 0
+        )
+        self.assertEqual(
+            search_resources(AnonymousUser(), "OtherTitle")["queryset"].count(), 0
+        )
 
     @authenticate
     def test_search_on_resources_public_data(self):
         ResourceFactory.create(title="MyTitle", is_public=True)
-        self.assertEqual(search_resources(authenticate.user, "MyTitle").count(), 1)
-        self.assertEqual(search_resources(authenticate.user, "OtherTitle").count(), 0)
+        self.assertEqual(
+            search_resources(authenticate.user, "MyTitle")["queryset"].count(), 1
+        )
+        self.assertEqual(
+            search_resources(authenticate.user, "OtherTitle")["queryset"].count(), 0
+        )
 
     @authenticate
     def test_search_on_resources_own_private_data(self):
         ResourceFactory.create(
             title="MyTitle", is_public=False, creator=authenticate.user
         )
-        self.assertEqual(search_resources(authenticate.user, "MyTitle").count(), 1)
-        self.assertEqual(search_resources(authenticate.user, "OtherTitle").count(), 0)
+        self.assertEqual(
+            search_resources(authenticate.user, "MyTitle")["queryset"].count(), 1
+        )
+        self.assertEqual(
+            search_resources(authenticate.user, "OtherTitle")["queryset"].count(), 0
+        )
 
     @authenticate
     def test_search_on_resources_others_private_data(self):
         ResourceFactory.create(title="MyTitle", is_public=False)
-        self.assertEqual(search_resources(authenticate.user, "MyTitle").count(), 0)
-        self.assertEqual(search_resources(authenticate.user, "OtherTitle").count(), 0)
+        self.assertEqual(
+            search_resources(authenticate.user, "MyTitle")["queryset"].count(), 0
+        )
+        self.assertEqual(
+            search_resources(authenticate.user, "OtherTitle")["queryset"].count(), 0
+        )
 
     def test_anonymous_search_on_bases_public_data(self):
         BaseFactory.create(title="MyTitle", is_public=True)
-        self.assertEqual(search_bases(AnonymousUser(), "MyTitle").count(), 1)
-        self.assertEqual(search_bases(AnonymousUser(), "OtherTitle").count(), 0)
+        self.assertEqual(
+            search_bases(AnonymousUser(), "MyTitle")["queryset"].count(), 1
+        )
+        self.assertEqual(
+            search_bases(AnonymousUser(), "OtherTitle")["queryset"].count(), 0
+        )
 
     def test_anonymous_search_on_bases_private_data(self):
         BaseFactory.create(title="MyTitle", is_public=False)
-        self.assertEqual(search_bases(AnonymousUser(), "MyTitle").count(), 0)
-        self.assertEqual(search_bases(AnonymousUser(), "OtherTitle").count(), 0)
+        self.assertEqual(
+            search_bases(AnonymousUser(), "MyTitle")["queryset"].count(), 0
+        )
+        self.assertEqual(
+            search_bases(AnonymousUser(), "OtherTitle")["queryset"].count(), 0
+        )
 
     @authenticate
     def test_search_on_bases_public_data(self):
         BaseFactory.create(title="MyTitle", is_public=True)
-        self.assertEqual(search_bases(authenticate.user, "MyTitle").count(), 1)
-        self.assertEqual(search_bases(authenticate.user, "OtherTitle").count(), 0)
+        self.assertEqual(
+            search_bases(authenticate.user, "MyTitle")["queryset"].count(), 1
+        )
+        self.assertEqual(
+            search_bases(authenticate.user, "OtherTitle")["queryset"].count(), 0
+        )
 
     @authenticate
     def test_search_on_bases_own_private_data(self):
         BaseFactory.create(title="MyTitle", is_public=False, owner=authenticate.user)
-        self.assertEqual(search_bases(authenticate.user, "MyTitle").count(), 1)
-        self.assertEqual(search_bases(authenticate.user, "OtherTitle").count(), 0)
+        self.assertEqual(
+            search_bases(authenticate.user, "MyTitle")["queryset"].count(), 1
+        )
+        self.assertEqual(
+            search_bases(authenticate.user, "OtherTitle")["queryset"].count(), 0
+        )
 
     @authenticate
     def test_search_on_bases_others_private_data(self):
         BaseFactory.create(title="MyTitle", is_public=False)
-        self.assertEqual(search_bases(authenticate.user, "MyTitle").count(), 0)
-        self.assertEqual(search_bases(authenticate.user, "OtherTitle").count(), 0)
+        self.assertEqual(
+            search_bases(authenticate.user, "MyTitle")["queryset"].count(), 0
+        )
+        self.assertEqual(
+            search_bases(authenticate.user, "OtherTitle")["queryset"].count(), 0
+        )
 
     def test_search_on_resources_multiple_fields(self):
         ResourceFactory.create(title="MyTitle", description="my description")
-        self.assertEqual(search_resources(authenticate.user, "description").count(), 1)
-        self.assertEqual(search_resources(authenticate.user, "MyTitle").count(), 1)
+        self.assertEqual(
+            search_resources(authenticate.user, "description")["queryset"].count(), 1
+        )
+        self.assertEqual(
+            search_resources(authenticate.user, "MyTitle")["queryset"].count(), 1
+        )
 
     def test_search_ignores_case(self):
         ResourceFactory.create(title="MyTitle", is_public=True)
-        self.assertEqual(search_resources(AnonymousUser(), "MyTitle").count(), 1)
-        self.assertEqual(search_resources(AnonymousUser(), "mytitle").count(), 1)
+        self.assertEqual(
+            search_resources(AnonymousUser(), "MyTitle")["queryset"].count(), 1
+        )
+        self.assertEqual(
+            search_resources(AnonymousUser(), "mytitle")["queryset"].count(), 1
+        )
+
+    def test_possible_tags(self):
+        tag = TagFactory.create()
+        resource = ResourceFactory.create(title="MyTitle", is_public=True)
+        self.assertListEqual(
+            list(search_resources(AnonymousUser(), "MyTitle")["possible_tags"]), []
+        )
+
+        resource.tags.add(tag)
+        self.assertListEqual(
+            list(search_resources(AnonymousUser(), "MyTitle")["possible_tags"]),
+            [tag.pk],
+        )
+
+
+class TestSearchView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.url = reverse("search-list")
+
+    def test_search_view(self):
+        tag = TagFactory.create()
+        resource = ResourceFactory.create(title="MyTitle", is_public=True)
+        res = self.client.post(
+            self.url,
+            {"data_type": "resources", "text": "MyTitle"},
+            content_type="application/json",
+        )
+        self.assertListEqual(res.json()["results"]["possibleTags"], [])
+
+        resource.tags.add(tag)
+        res = self.client.post(
+            self.url,
+            {"data_type": "resources", "text": "MyTitle"},
+            content_type="application/json",
+        )
+        self.assertListEqual(res.json()["results"]["possibleTags"], [tag.pk])
+
+    def test_pagination(self):
+        base = BaseFactory.create(is_public=True)
+        page_size = SearchView.pagination_class.page_size
+        for ix in range(page_size + 1):
+            ResourceFactory.create(
+                title=f"Resource {ix}", is_public=True, root_base=base
+            )
+        # page 1 is limited to page_size results
+        res = self.client.post(
+            f"{self.url}?page=1",
+            {"data_type": "resources", "text": "Resource"},
+            content_type="application/json",
+        )
+        self.assertEqual(res.json()["count"], page_size + 1)
+        self.assertEqual(len(res.json()["results"]["objects"]), page_size)
+
+        # page 2 only has one itme
+        res = self.client.post(
+            f"{self.url}?page=2",
+            {"data_type": "resources", "text": "Resource"},
+            content_type="application/json",
+        )
+        self.assertEqual(res.json()["count"], page_size + 1)
+        self.assertEqual(len(res.json()["results"]["objects"]), 1)
+
+        # also works without specifying the page
+        res = self.client.post(
+            f"{self.url}",
+            {"data_type": "resources", "text": "Resource"},
+            content_type="application/json",
+        )
+        self.assertEqual(res.json()["count"], page_size + 1)
+        self.assertEqual(len(res.json()["results"]["objects"]), 20)
--- main/views/search_view.py
+++ main/views/search_view.py
@@ -1,9 +1,9 @@
-from rest_framework.decorators import api_view
+from rest_framework import viewsets, mixins
 from rest_framework.exceptions import APIException
-from rest_framework.request import Request
-from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from telescoop_auth.serializers import AuthSerializer
 
+from main.models import Resource
 from main.search import search_resources, search_bases, search_users
 from main.serializers.base_resource_serializers import (
     ShortResourceSerializer,
@@ -25,21 +25,63 @@ class HideEmailUserSerializer(AuthSerializer):
         fields = (field for field in AuthSerializer.Meta.fields if field != "email")
 
 
-@api_view(["POST"])
-def search(request: Request, data_type):
-    text = request.data.get("text", "")
-    if data_type == "resources":
-        serializer = ShortResourceSerializer
-        search_function = search_resources
-    elif data_type == "bases":
-        serializer = ShortBaseSerializer
-        search_function = search_bases
-    elif data_type == "users":
-        serializer = UserSerializerForSearch
-        search_function = search_users
-    else:
-        raise APIException(f"Unknown data type ({data_type})")
-    data = serializer(many=True).to_representation(
-        search_function(request.user, text=text)
-    )
-    return Response({"data_type": data_type, "objects": data})
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = "page_size"
+    max_page_size = 20
+
+
+class SearchView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Resource.objects.all()
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        """Get search results for selected data type."""
+        text = self.request.data.get("text", "")
+        data_type = self.request.data["data_type"]
+        if data_type == "resources":
+            search_function = search_resources
+        elif data_type == "bases":
+            search_function = search_bases
+        elif data_type == "users":
+            search_function = search_users
+        else:
+            raise APIException(f"Unknown data type ({data_type})")
+        return search_function(self.request.user, text=text)
+
+    def get_serializer_class(self):
+        data_type = self.request.data["data_type"]
+        if data_type == "resources":
+            return ShortResourceSerializer
+        elif data_type == "bases":
+            return ShortBaseSerializer
+        elif data_type == "users":
+            return UserSerializerForSearch
+
+    def create(self, request, *args, **kwargs):
+        """We use create as the request is a POST."""
+        search_results = self.get_queryset()
+        queryset, possible_tags = (
+            search_results["queryset"],
+            search_results["possible_tags"],
+        )
+
+        queryset = self.filter_queryset(queryset)
+
+        page = self.paginate_queryset(queryset)
+
+        if page is None:
+            raise APIException("Missing pagination")
+
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(
+            {
+                "data_type": self.request.data["data_type"],
+                "objects": serializer.data,
+                "possible_tags": list(possible_tags),
+            }
+        )
