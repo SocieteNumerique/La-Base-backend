5cbc0b4912b9dc90648754e246b4d22e255334ce
alice.telescoop <alice@telescoop.fr>
2022-08-08 11:04:20 +0200

    feat (license): tests

--- main/factories.py
+++ main/factories.py
@@ -2,8 +2,10 @@ from typing import Optional
 
 from factory.django import DjangoModelFactory
 import factory
+
+from main.models import TextContent, ContentSection
 from main.models.user import User
-from main.models.models import Collection
+from main.models.models import Collection, LicenseText
 
 from main.models.models import Tag, TagCategory, Base, Resource
 from main.models.user import UserGroup
@@ -81,3 +83,39 @@ class UserGroupFactory(DjangoModelFactory):
         model = UserGroup
 
     name = factory.Faker("text", max_nb_chars=30)
+
+
+class LicenseTextFactory(DjangoModelFactory):
+    class Meta:
+        model = LicenseText
+        exclude = ("has_link", "has_name")
+
+    has_link = factory.Faker("boolean")
+    has_name = factory.Faker("boolean")
+    link = factory.Maybe("has_link", factory.Faker("url"), None)
+    name = factory.Maybe("has_link", factory.Faker("text", max_nb_chars=30), None)
+
+
+def order(nb_built):
+    return nb_built + 1
+
+
+class SectionFactory(DjangoModelFactory):
+    class Meta:
+        model = ContentSection
+
+    resource = factory.SubFactory(ResourceFactory)
+    title = factory.Faker("text", max_nb_chars=30)
+    order = factory.Sequence(order)
+
+
+class TextContentFactory(DjangoModelFactory):
+    class Meta:
+        model = TextContent
+
+    text = factory.Faker("paragraph")
+    title = factory.Faker("text", max_nb_chars=30)
+    resource = factory.SubFactory(ResourceFactory)
+    is_draft = False
+    section = factory.LazyAttribute(lambda o: SectionFactory(resource=o.resource))
+    order = factory.Sequence(order)
