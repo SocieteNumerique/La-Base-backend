38f3f6cd68ec48c1f3c6bd24e6068f6e7ccd9618
alice.telescoop <alice@telescoop.fr>
2022-10-05 14:38:15 +0200

    feat (image): install versatile image field

--- moine_back/settings.py
+++ moine_back/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "versatileimagefield",
     "hijack",
     "hijack.contrib.admin",
     "telescoop_auth",
