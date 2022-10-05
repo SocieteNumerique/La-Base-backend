651bbde0ce5978c39ef55a10344930537f18e00b
alice.telescoop <alice@telescoop.fr>
2022-10-05 16:04:35 +0200

    feat (image): model new resizable model

--- main/models/utils.py
+++ main/models/utils.py
@@ -1,8 +1,13 @@
+from functools import partial
 from io import BytesIO
+from mimetypes import guess_type
+from os.path import splitext
 
 from PIL import Image
 from django.core.files.storage import default_storage
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import models
+from versatileimagefield.fields import VersatileImageField
 
 
 class TimeStampedModel(models.Model):
@@ -19,19 +24,50 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
+def upload_image_location(instance, filename, cropped=False):
+    name, ext = splitext(filename)
+    return f'{name}{f"_cropped" if cropped else ""}{ext}'
+
+
 class ResizableImage(models.Model):
-    image = models.ImageField()
-    # these four values are percentages of the image dimensions
-    # scale = complete_size / cropped_size
-    # relative_position = position / complete_size
-    scale_x = models.DecimalField(null=True, blank=True, max_digits=5, decimal_places=2)
-    scale_y = models.DecimalField(null=True, blank=True, max_digits=5, decimal_places=2)
-    relative_position_x = models.DecimalField(
-        null=True, blank=True, max_digits=5, decimal_places=2
+    image = VersatileImageField(upload_to=upload_image_location)
+    cropped_image = VersatileImageField(
+        upload_to=partial(upload_image_location, cropped=True), editable=False
     )
-    relative_position_y = models.DecimalField(
-        null=True, blank=True, max_digits=5, decimal_places=2
-    )
+
+    relative_top = models.FloatField(default=0, blank=True)
+    relative_left = models.FloatField(default=0, blank=True)
+    relative_height = models.FloatField(default=1, blank=True)
+    relative_width = models.FloatField(default=1, blank=True)
+
+    def pil_box(self, width, height):
+        (left, upper, right, lower) = (
+            self.relative_left * width,
+            self.relative_top * height,
+            (self.relative_left + self.relative_width) * width,
+            (self.relative_top + self.relative_height) * height,
+        )
+        return left, upper, right, lower
+
+    def create_crop(self):
+        # thanks https://stackoverflow.com/a/52936682/14872075
+        img = Image.open(self.image.file.file)
+
+        cropped_img = img.crop(self.pil_box(img.width, img.height))
+        image_file = BytesIO()
+        cropped_img.save(image_file, img.format)
+        self.cropped_image.save(
+            self.image.name,
+            InMemoryUploadedFile(
+                image_file,
+                None,
+                self.image.name,
+                guess_type(self.image.name),
+                cropped_img.size,
+                None,
+            ),
+            save=False,
+        )
 
 
 def resize_image(image):
